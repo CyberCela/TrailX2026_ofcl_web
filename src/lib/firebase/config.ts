5dc5c9472af7195ca04515cff922c4d93c72341b
ravinduHV <ravinduhvimarshana@gmail.com>
2026-04-18 02:45:52 +0530

    sign in debug

--- src/lib/firebase/config.ts
+++ src/lib/firebase/config.ts
@@ -9,11 +9,19 @@ export const firebaseConfig = {
   appId: env("NEXT_PUBLIC_FIREBASE_APP_ID"),
 };
 
-export const isFirebaseConfigured = Boolean(
-  firebaseConfig.apiKey &&
-    firebaseConfig.authDomain &&
-    firebaseConfig.projectId &&
-    firebaseConfig.storageBucket &&
-    firebaseConfig.messagingSenderId &&
-    firebaseConfig.appId
-);
+const missingFirebaseKeys = [
+  ["NEXT_PUBLIC_FIREBASE_API_KEY", firebaseConfig.apiKey],
+  ["NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN", firebaseConfig.authDomain],
+  ["NEXT_PUBLIC_FIREBASE_PROJECT_ID", firebaseConfig.projectId],
+  ["NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET", firebaseConfig.storageBucket],
+  ["NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID", firebaseConfig.messagingSenderId],
+  ["NEXT_PUBLIC_FIREBASE_APP_ID", firebaseConfig.appId],
+]
+  .filter(([, value]) => !value)
+  .map(([key]) => key);
+
+export const isFirebaseConfigured = missingFirebaseKeys.length === 0;
+
+if (!isFirebaseConfigured) {
+  console.error("Firebase config missing:", missingFirebaseKeys.join(", "));
+}
